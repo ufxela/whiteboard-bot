ae138d15cd9e81b218da386c95248b541d6f072d
ufxela <46544566+ufxela@users.noreply.github.com>
2020-05-16 17:06:32 +0000

    Deploying to gh-pages from  - f2151991cff94564f8094042f5cec3d4953d41d0 🚀

---
+++ precache-manifest.28599d1398c7f239ac765887613cef85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d426dddf9608b3b714f16b49e66749b2",
+    "url": "/whiteboard-bot/index.html"
+  },
+  {
+    "revision": "7f56add4e78dacd0bec8",
+    "url": "/whiteboard-bot/static/css/main.a8de8a8c.chunk.css"
+  },
+  {
+    "revision": "a6afcb42c1519067e9c3",
+    "url": "/whiteboard-bot/static/js/2.e27d1785.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whiteboard-bot/static/js/2.e27d1785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f56add4e78dacd0bec8",
+    "url": "/whiteboard-bot/static/js/main.b64c98e7.chunk.js"
+  },
+  {
+    "revision": "f65e04acc71854668bd5",
+    "url": "/whiteboard-bot/static/js/runtime-main.3f256dcd.js"
+  }
+]);
